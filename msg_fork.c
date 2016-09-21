4eb9b12f3fcb9922549a5d50d4b2564d6bfc0992
Andiry Xu <jix024@cs.ucsd.edu>
2016-09-20 21:52:05 -0700

    Add msg_fork

---
+++ msg_fork.c
@@ -0,0 +1,98 @@
+#define _GNU_SOURCE
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<assert.h>
+#include<pthread.h>
+#include<sys/epoll.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+#include<fcntl.h>
+#include<stdbool.h>
+
+static const int CONTROL_LEN = CMSG_LEN(sizeof(int));
+
+void send_fd(int fd, int fd_to_send)
+{
+	struct iovec iov[1];
+	struct msghdr msg;
+	char buf[0];
+	struct cmsghdr cm;
+
+	iov[0].iov_base = buf;
+	iov[0].iov_len = 1;
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+
+	cm.cmsg_len = CONTROL_LEN;
+	cm.cmsg_level = SOL_SOCKET;
+	cm.cmsg_type = SCM_RIGHTS;
+	*(int *)CMSG_DATA(&cm) = fd_to_send;
+	msg.msg_control = &cm;
+	msg.msg_controllen = CONTROL_LEN;
+
+	sendmsg(fd, &msg, 0);
+}
+
+
+int recv_fd(int fd)
+{
+	struct iovec iov[1];
+	struct msghdr msg;
+	char buf[0];
+	struct cmsghdr cm;
+	int fd_to_read;
+
+	iov[0].iov_base = buf;
+	iov[0].iov_len = 1;
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+
+	msg.msg_control = &cm;
+	msg.msg_controllen = CONTROL_LEN;
+
+	recvmsg(fd, &msg, 0);
+
+	fd_to_read = *(int *)CMSG_DATA(&cm);
+	return fd_to_read;
+}
+
+int main(void)
+{
+	int pipefd[2];
+	int fd_to_pass = 0;
+	int ret = socketpair(PF_UNIX, SOCK_DGRAM, 0, pipefd);
+	pid_t pid;
+
+	pid = fork();
+
+	if (pid == 0) {
+		close(pipefd[0]);
+		fd_to_pass = open("test.txt", O_RDWR | O_CREAT, 0666);
+		write(fd_to_pass, "qwr\0", 4);
+		send_fd(pipefd[1], fd_to_pass);
+		close(fd_to_pass);
+		exit(0);
+	} else {
+		char buf[1024];
+		memset(buf, '\0', 1024);
+		close(pipefd[1]);
+		fd_to_pass = recv_fd(pipefd[0]);
+		read(fd_to_pass, buf, 1024);
+		printf("I got fd %d and data %s\n", fd_to_pass, buf);
+		close(fd_to_pass);
+	}
+
+	return 0;
+}
+
+
+
